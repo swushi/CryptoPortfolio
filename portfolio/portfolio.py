d842c6df028d183ec09f6fcaad2a29d1b906fc13
Sam <samautrey7@gmail.com>
2018-05-13 02:29:28 -0500

    detailed title

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,3 +1,9 @@
+# This program will be track the my portfolio
+# and give me useful data such as the current 
+# value and percent change
+# Python version 3.6.5
+# Plan to add QT GUI integration eventually
+
 # Take in API
 
 # Show Coins to user
