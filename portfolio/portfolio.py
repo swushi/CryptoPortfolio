c353d8f43e6d20a64edda93f0d8ec83f65dd7c7f
Sam <samautrey7@gmail.com>
2018-05-13 03:08:21 -0500

    output coins to user

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -4,9 +4,22 @@
 # Python version 3.6.5
 # Plan to add QT GUI integration eventually
 
+import json
+import requests
+
 # Take in API
+baseURL = "http://coincap.io/"
+coins_api = requests.get(baseURL + "coins").text
+front_api = requests.get(baseURL + "front").text
+
+# Put into JSON
+coins = json.loads(coins_api)
+front = json.loads(front_api)
 
 # Show Coins to user
+for coin in front:
+    print('{:<20}{:.2f}'.format(coin['long'], coin['price']))
+
 
 # Ask for which coin they have
 
